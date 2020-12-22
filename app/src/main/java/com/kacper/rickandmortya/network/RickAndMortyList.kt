83783e15d1bad571bd9271f95b53758406b24f64
KacperL98 <kacperlitwinow517@gmail.com>
2020-12-22 16:18:09 +0100

    Delete unnecessary "?"

--- app/src/main/java/com/kacper/rickandmortya/network/RickAndMortyList.kt
+++ app/src/main/java/com/kacper/rickandmortya/network/RickAndMortyList.kt
@@ -4,11 +4,11 @@ data class RickAndMortyList(val results: ArrayList<CharacterData>)
 
 data class CharacterData(
     val id: Int,
-    val name: String?,
-    val species: String?,
-    val image: String?,
+    val name: String,
+    val species: String,
+    val image: String,
     val status: String,
-    val gender: String?,
+    val gender: String,
     val origin: Origin,
     val location: Location
 )
